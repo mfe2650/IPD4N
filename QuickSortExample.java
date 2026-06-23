182b25cec0d420b5b6a468b63206d42397371eaa
mfe2650 <163406658+mfe2650@users.noreply.github.com>
2026-06-23 15:51:59 +0900

    Add files via upload

---
+++ QuickSortExample.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class QuickSortExample {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {29, 10, 14, 37, 13, 5, 42, 18};
+
+        System.out.println("Before sort: " + Arrays.toString(data));
+        quickSort(data, 0, data.length - 1);
+        System.out.println("After sort : " + Arrays.toString(data));
+    }
+}
